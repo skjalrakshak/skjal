22ab208b787eb88a486f03efae81da59a997eec7
koushiknagabhatla-ctrl <koushiknagabhatla@gmail.com>
2026-05-17 14:41:19 +0530

    fix: add Cloudinary to CSP img-src to allow images to load

--- server.js
+++ server.js
@@ -82,7 +82,7 @@ function securityHeaders(contentType) {
       "script-src 'self' 'unsafe-inline' https://cdnjs.cloudflare.com https://unpkg.com https://cdn.tailwindcss.com",
       "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
       "font-src 'self' https://fonts.gstatic.com",
-      "img-src 'self' data: https://randomuser.me https://images.unsplash.com",
+      "img-src 'self' data: https://randomuser.me https://images.unsplash.com https://res.cloudinary.com",
       "connect-src 'self'",
       "object-src 'none'",
       "base-uri 'self'",
