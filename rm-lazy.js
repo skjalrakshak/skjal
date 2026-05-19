ede9fa922910259b5e4bb547fe79fec0532fd06a
koushiknagabhatla-ctrl <koushiknagabhatla@gmail.com>
2026-05-19 22:06:08 +0530

    fixeddd3

--- rm-lazy.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-['index.html', 'jala-rakshak.html', 'shield.html'].forEach(file => {
-  let content = fs.readFileSync(file, 'utf8');
-  // Remove loading="lazy"
-  content = content.replace(/ loading="lazy"/g, '');
-  // Optionally, we can add fetchpriority="high" to the very first image like the app mockup or hero images, but removing lazy will prevent the late loading.
-  fs.writeFileSync(file, content);
-});
-console.log('Lazy loading removed');
